f949fc02df3c19619fe75b3f559a668da850a01e
leadustin <155164500+leadustin@users.noreply.github.com>
2025-09-10 21:44:35 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,77 @@
 import React, { useState } from 'react';
-import './App.css';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useGameState } from './hooks/useGameState'; 
 import { StartScreen } from './components/start_screen/StartScreen';
 import { CharacterCreationScreen } from './components/character_creation/CharacterCreationScreen';
-import { GameView } from './components/game_view/GameView';
+import GameView from './components/game_view/GameView';
+import CharacterSheet from './components/character_sheet/CharacterSheet';
+import './App.css';
 
 function App() {
-  const [gameState, setGameState] = useState('START_SCREEN');
-  const [character, setCharacter] = useState(null);
+  const {
+    gameState,
+    handleNewGame,
+    handleLoadGame,
+    handleSaveGame,
+    handleDeleteGame,
+    handleCharacterCreation,
+  } = useGameState();
 
-  const handleCharacterFinalized = (finalizedCharacter) => {
-    setCharacter(finalizedCharacter);
-    setGameState('WORLD_MAP');
-  };
-  
-  const handleLoadGame = (loadedCharacter) => {
-    setCharacter(loadedCharacter);
-    setGameState('WORLD_MAP');
+  const [showCharacterSheet, setShowCharacterSheet] = useState(false);
+
+  const toggleCharacterSheet = () => {
+    setShowCharacterSheet(prevState => !prevState);
   };
 
-  // Die Wrapper-Divs wurden entfernt
+  const renderScreen = () => {
+    switch (gameState.screen) {
+      case 'start':
+        return (
+          <StartScreen
+            onNewGame={handleNewGame}
+            onLoadGame={handleLoadGame}
+            onSaveGame={handleSaveGame}
+            onDeleteGame={handleDeleteGame}
+            isGameLoaded={!!gameState.character}
+            saveFileExists={localStorage.getItem('gameState') !== null}
+          />
+        );
+      case 'character-creation':
+        // KORREKTUR HIER: 'onCharacterFinalized' statt 'onFinish'
+        return <CharacterCreationScreen onCharacterFinalized={handleCharacterCreation} />;
+      case 'game':
+        return (
+          <GameView
+            character={gameState.character}
+            onToggleCharacterSheet={toggleCharacterSheet}
+          />
+        );
+      default:
+        return (
+            <StartScreen
+              onNewGame={handleNewGame}
+              onLoadGame={handleLoadGame}
+              onSaveGame={handleSaveGame}
+              onDeleteGame={handleDeleteGame}
+              isGameLoaded={!!gameState.character}
+              saveFileExists={localStorage.getItem('gameState') !== null}
+            />
+          );
+    }
+  };
+
   return (
-    <div className="App"> 
-      {gameState === 'START_SCREEN' && <StartScreen onNewGame={() => setGameState('CHARACTER_CREATION')} onLoadGame={handleLoadGame} />}
-      {gameState === 'CHARACTER_CREATION' && <CharacterCreationScreen onCharacterFinalized={handleCharacterFinalized} />}
-      {gameState === 'WORLD_MAP' && character && <GameView character={character} />}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      {/* Dieser .App Container zentriert alles */}
+      <div className="App">
+        {/* Dieser .game-container begrenzt die Breite */}
+        <div className="game-container">
+          {renderScreen()}
+          {showCharacterSheet && <CharacterSheet onClose={toggleCharacterSheet} />}
+        </div>
+      </div>
+    </DndProvider>
   );
 }
 
