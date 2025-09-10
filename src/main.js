fcc71611cb3e6257ed74674d178efbc96e0246cb
leadustin <155164500+leadustin@users.noreply.github.com>
2025-09-10 16:10:55 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,12 +5,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// Erstelle die Vue-App-Instanz
 const app = createApp(App)
 
-// Binde Pinia und den Router an die App
-app.use(createPinia()) // State Management aktivieren
-app.use(router) // Routing aktivieren
+app.use(createPinia())
+app.use(router)
 
-// Hänge die App an das DOM-Element mit der ID 'app' an
 app.mount('#app')
