bcb72c2f9650fe8b19a9e2f8a4172840bc0e2793
leadustin <155164500+leadustin@users.noreply.github.com>
2025-09-10 16:07:31 +0200

    Add InventorySlot component for item display

---
+++ src/components/character_sheet/InventorySlot.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inventory-slot">
+    <div v-if="item && !item.isEmpty" class="item-display">
+      {{ item.name.charAt(0) }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  item: { type: Object, default: null },
+})
+</script>
+
+<style scoped>
+.inventory-slot {
+  width: 60px;
+  height: 60px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border: 1px solid #444;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5em;
+  color: #fff;
+  cursor: grab;
+}
+.inventory-slot:active {
+  cursor: grabbing;
+}
+.item-display {
+  padding: 5px;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
