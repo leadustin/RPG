75f9670f6bca06af0b3cadc73a33da865955bd8e
leadustin <155164500+leadustin@users.noreply.github.com>
2025-09-11 16:38:21 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useGameState } from './hooks/useGameState'; 
-import { StartScreen } from './components/start_screen/StartScreen';
-import { CharacterCreationScreen } from './components/character_creation/CharacterCreationScreen';
-import GameView from './components/game_view/GameView';
-import CharacterSheet from './components/character_sheet/CharacterSheet';
-import './App.css';
+import React, { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { useGameState } from "./hooks/useGameState";
+import { StartScreen } from "./components/start_screen/StartScreen";
+import { CharacterCreationScreen } from "./components/character_creation/CharacterCreationScreen";
+import GameView from "./components/game_view/GameView";
+import CharacterSheet from "./components/character_sheet/CharacterSheet";
+import "./App.css";
 
 function App() {
   const {
@@ -21,12 +21,12 @@ function App() {
   const [showCharacterSheet, setShowCharacterSheet] = useState(false);
 
   const toggleCharacterSheet = () => {
-    setShowCharacterSheet(prevState => !prevState);
+    setShowCharacterSheet((prevState) => !prevState);
   };
 
   const renderScreen = () => {
     switch (gameState.screen) {
-      case 'start':
+      case "start":
         return (
           <StartScreen
             onNewGame={handleNewGame}
@@ -34,13 +34,16 @@ function App() {
             onSaveGame={handleSaveGame}
             onDeleteGame={handleDeleteGame}
             isGameLoaded={!!gameState.character}
-            saveFileExists={localStorage.getItem('gameState') !== null}
+            saveFileExists={localStorage.getItem("gameState") !== null}
           />
         );
-      case 'character-creation':
-        // KORREKTUR HIER: 'onCharacterFinalized' statt 'onFinish'
-        return <CharacterCreationScreen onCharacterFinalized={handleCharacterCreation} />;
-      case 'game':
+      case "character-creation":
+        return (
+          <CharacterCreationScreen
+            onCharacterFinalized={handleCharacterCreation}
+          />
+        );
+      case "game":
         return (
           <GameView
             character={gameState.character}
@@ -49,26 +52,30 @@ function App() {
         );
       default:
         return (
-            <StartScreen
-              onNewGame={handleNewGame}
-              onLoadGame={handleLoadGame}
-              onSaveGame={handleSaveGame}
-              onDeleteGame={handleDeleteGame}
-              isGameLoaded={!!gameState.character}
-              saveFileExists={localStorage.getItem('gameState') !== null}
-            />
-          );
+          <StartScreen
+            onNewGame={handleNewGame}
+            onLoadGame={handleLoadGame}
+            onSaveGame={handleSaveGame}
+            onDeleteGame={handleDeleteGame}
+            isGameLoaded={!!gameState.character}
+            saveFileExists={localStorage.getItem("gameState") !== null}
+          />
+        );
     }
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      {/* Dieser .App Container zentriert alles */}
       <div className="App">
-        {/* Dieser .game-container begrenzt die Breite */}
         <div className="game-container">
           {renderScreen()}
-          {showCharacterSheet && <CharacterSheet onClose={toggleCharacterSheet} />}
+          {/* ✅ HIER: character-Prop hinzugefügt */}
+          {showCharacterSheet && (
+            <CharacterSheet
+              character={gameState.character} // ✅ character übergeben
+              onClose={toggleCharacterSheet}
+            />
+          )}
         </div>
       </div>
     </DndProvider>
