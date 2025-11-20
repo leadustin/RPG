4f2dcbfe5dd7ea56c345c43d5ae8796ca35d91ac
leadustin <155164500+leadustin@users.noreply.github.com>
2025-11-20 15:58:46 +0100

    no mini

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,24 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+
   // Dieser Block ist die Lösung für das "react-rnd"-Problem
   optimizeDeps: {
     include: [
       'react-rnd',
-      'clsx' 
+      'clsx'
     ],
   },
-  
+
   // Optional: Setzt den Port wieder auf 3000, wie du es gewohnt bist
   server: {
     port: 3000,
   },
+
+  // ➤ Minify deaktivieren (JS und CSS)
+  build: {
+    minify: false,      // JS nicht minifizieren
+    cssMinify: false,   // CSS nicht minifizieren (Vite 5+)
+    sourcemap: true,
+  }
 });
